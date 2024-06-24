d47aa4bce76fb28ccfdfac99fc8b11db38482db3
QuixaG <66678629+QuixaG@users.noreply.github.com>
2024-06-24 17:33:58 -0300

    Update config.php

--- config.php
+++ config.php
@@ -5,3 +5,13 @@
     define( 'BASE', 'cadastro');
 
     $conn = new MySQLi(HOST,USER,PASS,BASE);
+
+    if ($conn->connect_error) {
+        die("Erro na conexão com o banco de dados: " . $conn->connect_error);
+    }
+    
+    // Verifique a seleção do banco de dados
+    if (!$conn->select_db(BASE)) {
+        die("Erro na seleção do banco de dados: " . $conn->error);
+    }
+?>
